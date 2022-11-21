4949ef64c459e8c9ead8568eba95b2fdf8e8ca7a
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2022-11-21 12:09:05 +0600

    spring-init

---
+++ src/main/java/com/passingtest/PassingtestApplication.java
@@ -0,0 +1,13 @@
+package com.passingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassingtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassingtestApplication.class, args);
+	}
+
+}

c412a51bc88153e355628e37bf1e2a7fcaf4baca
sismailov <sanzhar.ismailov@ocado.com>
2022-11-23 05:29:33 +0100

    P4 - AnswerService

--- src/main/java/com/passingtest/service/AnswerService.java
+++ src/main/java/com/passingtest/service/AnswerService.java
@@ -1,8 +1,11 @@
 package com.passingtest.service;
 
+import com.passingtest.exception.ObjectNotFoundException;
 import com.passingtest.model.entity.Answer;
 import com.passingtest.model.entity.Question;
 import com.passingtest.repository.AnswerRepository;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,9 @@ public class AnswerService {
     @Autowired
     AnswerRepository answerRepository;
 
-    public Answer getAnswerById(int id) {
-        return answerRepository.findById(id).get();
+    public Answer getAnswerById(int id) throws ObjectNotFoundException {
+        return answerRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(id, Answer.class));
     }
 
     public void saveOrUpdate(Answer answer) {
@@ -22,10 +26,44 @@ public class AnswerService {
     }
 
     public List<Answer> getAnswersByQuestionId(Integer questionId) {
-        return null;
+        return answerRepository.findByAnswersByQuestionId(questionId);
     }
 
     public boolean isSelectedQuestionAnswersIsCorrect(Question question, List<Answer> selectedAnswers) {
-        return false;
+        if (selectedAnswers.isEmpty()) {
+            throw new RuntimeException("Как минимум 1 ответ должен быть выбран!");
+        }
+        //before java 8
+/*
+
+        List<Answer> correctAnswers = question.getAnswers().stream()
+                .filter(Answer::getCorrect)
+                .collect(Collectors.toList());
+        if (correctAnswers.size() != selectedAnswers.size()) {
+            return false;
+        }
+        for (int i = 0; i < correctAnswers.size(); i++) {
+            for (int j = 0; j < selectedAnswers.size(); j++) {
+                Answer selectedAnswer = selectedAnswers.get(j);
+                if (selectedAnswer.getId().equals(correctAnswers.get(i).getId())) {
+                    break;
+                }
+                if (j == selectedAnswers.size() - 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+*/
+        long correctAnswersCount = question.getAnswers().stream().filter(Answer::getCorrect).count();
+        return correctAnswersCount == selectedAnswers.size() &&
+                question.getAnswers().stream()
+                        .filter(Answer::getCorrect)
+                        .map(Answer::getId)
+                        .anyMatch(correctAnswerId ->
+                                selectedAnswers.stream().map(Answer::getId).distinct()
+                                        .anyMatch(selectedAnswerId -> selectedAnswerId.equals(correctAnswerId))
+                        );
+
     }
 }
